05782ca804ec41cc37ae73a154f905b384acdb53
wilailux <lllwilailux@gmail.com>
2016-09-30 16:32:18 +0700

    edit file name MainFragment2

--- app/src/main/java/com/augmentis/ayp/mymovie/MainActivity.java
+++ app/src/main/java/com/augmentis/ayp/mymovie/MainActivity.java
@@ -6,6 +6,6 @@ public class MainActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment onCreateFragment() {
-        return new MainFragment();
+        return new MainFragment2();
     }
 }
